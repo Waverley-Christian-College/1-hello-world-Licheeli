c19de77bf1290e78c6150cf2180d1ff1ea8f22cb
Licheeli <wassupeliot@gmail.com>
2026-02-17 04:05:34 +0000

    created password vault

---
+++ password.py
@@ -0,0 +1,6 @@
+password = input("Whats the Password?: ")
+name = input("Whats your name?: ")
+if password == "I am very smart":
+    print(f"Welcome in {name}.")
+else:
+    print("Do i know you?")
